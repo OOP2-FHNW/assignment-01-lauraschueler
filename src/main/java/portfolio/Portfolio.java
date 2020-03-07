1169c9ae9b48c06f2fc73dee9996786b0cd20509
lauraschueler <laura.schueler@students.fhnw.ch>
2020-03-07 16:11:01 +0100

    assignment-01

--- src/main/java/portfolio/Portfolio.java
+++ src/main/java/portfolio/Portfolio.java
@@ -1,5 +1,48 @@
 package portfolio;
 
-public class Portfolio {
+import portfolio.investments.Investment;
+
+import java.util.ArrayList;
+
+public class Portfolio<T extends Investment> {
     //TODO Complete this implementation and make it generic
+
+    ArrayList<T> investments = new ArrayList<>();
+
+    public boolean contains(T investment) {
+        if (this.getShare(investment.getTitel()) != null) {
+            return true;
+        }
+        return false;
+    }
+
+    public void buy(T investment) {
+        if (!this.investments.contains(investment)) {
+            this.investments.add(investment);
+        } else {
+            // investments contains investment
+            this.getShare(investment.getTitel()).setCount(investment.getCount()+1);
+        }
+    }
+
+    public void sell(String titel, int count) {
+        T investment = this.getShare(titel);
+        if (investment != null) {
+            if (investment.getCount() <= count) {
+                investment.setCount(0);
+                investments.remove(investment);
+            } else {
+                investment.setCount(investment.getCount() - count);
+            }
+        }
+    }
+
+    public T getShare(String titel) {
+        for (T t : investments) {
+            if (t.getTitel().equals(titel)) {
+                return t;
+            }
+        }
+        return null;
+    }
 }
